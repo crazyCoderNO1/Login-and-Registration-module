ae13b2c874c5ed09293556dd945f225193829bdd
Carroll <me@luoerming.com>
2017-05-29 03:51:59 +0800

    Initial Commit

---
+++ Login and Registration module/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Login and Registration module
+//
+//  Created by Carroll on 2017/5/29.
+//  Copyright © 2017年 Carroll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Login and Registration moduleTests/Login_and_Registration_moduleTests.m
@@ -0,0 +1,39 @@
+//
+//  Login_and_Registration_moduleTests.m
+//  Login and Registration moduleTests
+//
+//  Created by Carroll on 2017/5/29.
+//  Copyright © 2017年 Carroll. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Login_and_Registration_moduleTests : XCTestCase
+
+@end
+
+@implementation Login_and_Registration_moduleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
